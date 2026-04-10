9ee3d4d2c04fea318d6fe16dc61633653f39d366
NITESH-JAT <njat7249@gmail.com>
2026-04-10 14:19:53 +0530

    updated backend for leaderboard

--- src/modules/user/userService.ts
+++ src/modules/user/userService.ts
@@ -1,6 +1,6 @@
-//src/modules/user/userService.ts
 import * as userRepo from './userRepository';
 
 export const fetchLeaderboard = async () => {
-  return await userRepo.getTopUsersByPoints(100);
+  // Removed the '100' argument so it matches the repository function!
+  return await userRepo.getTopUsersByPoints();
 };
